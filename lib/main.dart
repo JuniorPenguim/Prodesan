1e8de71e48f5a09153ae560c119c7782abb3e653
Fábio Júnior <fjumisan@gmail.com>
2021-12-10 10:31:57 -0300

    Inicio StateFulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,54 @@
 
 import 'package:flutter/material.dart';
 
-main(){
-  
-  runApp(AppWidget(title: 'Eh nooooix',)); //Função Principal Global do Flutter
+main() {
+  runApp(AppWidget(
+    title: 'Eh nooooix',
+  )); //Função Principal Global do Flutter
 }
 
-class AppWidget extends StatelessWidget{ //Widget sem estado/imutável
-  
-  
-  final String title;
+/////// STATELESS WIDGET ///////
+class AppWidget extends StatelessWidget {
+  //Widget sem estado/imutável
 
+  final String title;
   const AppWidget({Key? key, required this.title}) : super(key: key);
-  
-  
-  
-  
+
   @override
   Widget build(BuildContext context) {
-    
-    return(Container( 
-    child: Center(
-      child: Text(
-        title,
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 100.0),
-      ),
-    ),
-    ));
-    
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: HomePage(),
+    );
   }
+}
 
+/////// STATEFUL WIDGET ///////
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
 
+/////// STATE DO STATEFUL WIDGET ///////
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: GestureDetector(
+        //Função identificadora de toques
+        child: Text('Eh noix $counter vezes'),
+        onTap: () {
+          setState(() {
+            //Função de atualizar state/rebuild
+            counter++;
+          });
+        },
+      ),
+    ));
+  }
 }
